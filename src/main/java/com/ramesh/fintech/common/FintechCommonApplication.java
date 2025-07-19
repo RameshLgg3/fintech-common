8d5539848635543b455d12e52ba65a2b4ae6f9f6
RameshLgg3 <rameshzce@gmail.com>
2025-07-19 09:27:27 +0530

    Initial project setup for fintech-common

---
+++ src/main/java/com/ramesh/fintech/common/FintechCommonApplication.java
@@ -0,0 +1,13 @@
+package com.ramesh.fintech.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintechCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintechCommonApplication.class, args);
+	}
+
+}
